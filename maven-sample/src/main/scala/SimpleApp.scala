e129afc8d37a1c4795b6cee313803f217101d1b7
Petr Kozelka <pkozelka@gmail.com>
2017-07-17 16:54:12 +0200

    reformat + use arg 0 for file name

--- maven-sample/src/main/scala/SimpleApp.scala
+++ maven-sample/src/main/scala/SimpleApp.scala
@@ -3,8 +3,11 @@ import org.apache.spark.sql.SparkSession
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "pom.xml" // Should be some file on your system
-    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val logFile = args(0) // Should be some file on your system
+    val spark = SparkSession
+      .builder
+      .appName("Simple Application")
+      .getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
